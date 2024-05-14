972ca607593151e16c2a4ea0bb6e5ff60d97b198
Hengker EpEp <167304514+THEOYS123@users.noreply.github.com>
2024-05-14 22:12:33 +0700

    Create apalah.sh

---
+++ apalah.sh
@@ -0,0 +1,71 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+
+
+
+echo -e $red"
+
+          ⣿⣿⣿⣿⡿⠳⠑⠀⠀⠀⠱⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠓⠀⠀⠀⠑⠳⣷⣿⣿⣿⣿ 
+          ⣿⣿⣿⢗⠌⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿⣿⠷⠑⠀⠀⠑⡳⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⣀⡹⣿⣿⣿ 
+          ⣿⣿⣃⠒⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⢃⠀⠀⠀⠀⠀⠀⠰⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⡡⠼⣿⣿ 
+          ⣿⠏⠿⠁⠀⠀⠀⠀⠀⠀⠀⣡⣱⣿⡿⠃⠎⠀⠀⠀⠀⠀⠀⠀⠸⣷⣿⠟⠎⠀⠀⠀⠀⠀⠀⠀⠐⣣⣰⣿ 
+          ⣿⣰⠁⠀⠀⠀⠀⠀⠀⠀⠀⠐⢏⠷⡨⣳⠀⠀⠀⠀⠀⠀⠀⠀⠑⣂⡳⡸⠀⠀⠀⠀⠀⠀⠀⠀⠀⡰⠎⣿ 
+          ⣿⣰⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⢀⠏⠐⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⢌⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠏⣿ 
+          ⣿⠰⠀⣄⣌⠀⠀⡶⠷⠀⠀⢀⡬⠁⠑⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⣆⠈⠀⠀⡳⡧⠀⠀⣌⡌⠀⠇⣿ 
+          ⣿⠏⠀⠀⠀⠀⠀ ⠀⠀⠀⣾⠀⠀⠀⠀⣀⢈⠀⠀⠀⠀⢈⠌⠀⠀⠀⠐⣃⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿ 
+          ⣿⣯⠀⠀⠀⠀⠀⠀⠀⠀⠀⡳⠀⠀⠀⠀⠀⠱⠳⠂⠠⠳⠓⠀⠀⠀⠀⢀⠾⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿ 
+          ⣿⣿⣯⠈⠀⠀⠀⠀⢀⡄⠒⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡈⠒⠀⠡⠄⠈⠀⠀⠀⠀⢀⣾⣿⣿ 
+          ⣿⣿⡷⠷⠈⠀⡤⠂⠁⠀⠀⢈⣌⠌⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡈⠒⣀⣌⢀⠀⠀⠐⠢⡄⠀⢀⠳⡷⣿⣿ 
+          ⡁⣢⠞⠠⠏⠀⠀⠀⠀⠀⣰⣷⠀⠁⠀⠀⠀⠀⠀⠀⡄⠀⠂⠁⠀⠀⠑⠀⠿⠎⠀⠀⠀⠀⠀⣲⠀⣡⠦⠔ 
+         ⠀⠄⠁⠀⠀⠀⠀⠀⠀⣀⢔⢂⠀⠀⠀⠀⢈⢈⠀⠀⠀⠀⢀⢈⠀⠀⠀⠀⣼⡈⠌⠀⠀⠀⠀⠁⠀⠐⡀⠃ 
+          ⠿⠀⠀⠀⠀⠀⠀⣀⡆⣸⠀⠐⠁⠀⠀⠀⠀⠐⠑⠑⠑⠑⠑⠀⠀⠀⠀⠐⠁⠀⢏⡤⠌⠀⠀⠀⠀⠀⠁⣱ 
+          ⢫⢈⢈⢈⢈⢈⢈⢋:⢹⢈⢈⢈⢈⢈⢈⢈⢈⢈⢈⢈⢈⢈⢈⢈⢈⢈⢈⢈⢈⢉⢈⢹⢈⢈⢈⢈⢈⢈⢸ 
+          ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ 
+          ⣿⣿⠓⠑⣿⡿⡷⡷⡷⣿⡿⡷⡷⣿⡿⡷⡷⣷⣿⠓⣤⠌⣹⡿⡷⡷⣿⡿⡷⡷⣿⠑⣱⡷⠓⣷⡿⡷⡷⣷ 
+          ⣿⠇⡼⠀⣳⠏⢀⠏⠀⠿⠀⣼⠀⣰⠁⣠⠏⣀⣯⢈⠐⠱⠿⠀⠶⢈⠿⠀⣾⣌⠏⠀⣾⠁⣠⠟⠀⡿⠀⣾ 
+          ⠟⣨⣮⠈⣰⠀⣰⠏⠠⣏⠈⠷⢈⣾⠀⣸⠈⣰⢏⡰⠇⣈⣏⠈⠳⣹⢏⠀⠳⣹⠋⢀⣿⠀⣰⢏⠰⢇⠀⣿ 
+
+
+     "
+
+     
+     
+echo -e $red"                            [ KUMPULAN KUMPULAN SCRIP TERMUX INDONESIA]"
+echo -e $green"
+[1].SPAM WA               [6].FIX SIGNAL 19 KALI LINUX  [11].UPDATE SCRIP HOZOO                [16].PANDORA BY MRSANZz
+[2].DDOS SILVER           [7].PHONEINFOGA OSINT         [12].LAPOR BUG SC ERROR HOZOO ?        [17].TOOLS4 BY GALIRUS 
+[3].DEFACE                [8].TRACK IP   OSINT          [13].YOUTUBE :SYSTEM UMBRELLA DRAK999  [18].TOOLS SPAM WA BY VIDRA 
+[4].SPAM CALL WA          [9].TRACK PHONE OSINT         [14].TIKTOK : SYSTEM UMBRELLA DRAK999  [19].PHIZHER LINK PISING 
+[5].INSTALLL KALI LINUX   [10].HACK PISING PIZHER       [15].TOOLS BANNER TERMUX               [20].CAM  PIZHER HACK 
+[21].SADAP CAMERA         [26].INSTALL UBUNTU KDE       [31].TOOLS 18 KALMENIX                 [36].TOOLS DESKTOP BY BYJHONZ
+[22].TOOLS owl oisnt      [27].INSTALL PKG MODULE TERMUX[32].TOOLS  🔰 DevilX 🔰               [37].TOOLS OSINT BY JHONZ
+[23].SPAM WA VIIP         [28].HACK CCTV                [33].TOOLS 🔰 WORDGEN 🔰               [38].TOOLS LACAK BY JHONZ
+[24].JARVIS BY GALIRUS    [29].TERMUX BACKUP            [34].TOOLS 🔰FACEBOOK HACK🔰           [39].TOOLS OSINT VIIP
+[25].VIRUS TIKTOK BY HOZOO[30].🔰 Facebook Bruteforce 🔰[35].🔰 LORDPHISH 🔰                   [40].TOOLS OSINT GhostTrack
+[41]. Ubuntu mod.         [42].install desktop Ubuntu mod
+
+"
+read -p "                            SYSTEM UMBRELLA DRAK999-->" HOZOO;
+#Spam OTP
+if [ $HOZOO = 01 ] || [ $HOZOO = 1 ]
+    then
+    clear
+    echo -e $green" Installing..."
+    sleep 1
+apt update -y && apt upgrade -y
+apt install git -y
+apt install python -y
+git clone https://github.com/FatihArridho/brutal.git
+cd brutal
+pip install -r install.txt
+python brutal.py
+  clear
+    echo -e $green"Done Installed..."
+fi
